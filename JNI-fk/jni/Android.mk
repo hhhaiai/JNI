0a37a0147c12b5f9a63c21d26298e186d9c177f5
sanbo <sanbo.xyz@gmail.com>
2018-09-23 00:09:09 +0800

    增加P反射.

---
+++ JNI-fk/jni/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE := art
+LOCAL_SRC_FILES := art.cpp \
+	main.cpp
+
+
+#下面两种方式均是使用log方法,任选一句即可
+#LOCAL_LDLIBS := -llog
+LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog
+#下面一句是编译成动态库，即so库
+include $(BUILD_SHARED_LIBRARY)
+
+LOCAL_PATH := $(call my-dir)
